e4b960d0d4f79ba8de361df565b23684d9e03edd
fburatta <fabrizio@moldiscovery.com>
2019-07-19 12:26:17 +0200

    aggiustate le eccezioni per il nuovo app

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,49 +2,83 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 import jbook.util.Input;
 import java.util.*;
 
 public class MainClass {
-	
-	public static Appuntamento nuovoApp(){
-		
-		System.out.print("Inserisci il nome della persona:");
-		String n = Input.readString();
-		
-		System.out.print("Inserisci la data (PER ORA AAAA-MMM-GG):");
-		String da = Input.readString();
-		String arr[];
-		arr=da.split("-");
-		
-		System.out.print("Inserisci la durata (in minuti):");
-		int du = Input.readInt();
-		
-		System.out.print("Inserisci luogo:");
-		String l = Input.readString();
-		
-		System.out.print("Inserisci orario:");
-		String o = Input.readString();
-		String arr2[];
-		arr2=o.split(":");
-		
-		
-		Appuntamento app = new Appuntamento();
-		app.setNome(n);
-		app.setData(arr[0], arr[1], arr[2]);
-		app.setDurata(du);
-		app.setLuogo(l);
-		app.setOrario(Integer.parseInt(arr2[0]), Integer.parseInt(arr2[1]));
-		return app;
+
+	public static void stampaMenu(Boolean giaesistente) {
+		if (giaesistente) {
+			System.out.println("\n\n Premi un tasto per tornare al menu");
+			Input.readString();
+		}
+		System.out.println("\n\n GESTIONE RUBRICA DI APPUNTAMENTI");
+		System.out.println("\n\n");
+		System.out.println("1 - Aggiungi un appuntamento");
+		System.out.println("2 - Elimina un appuntamento (tutti i dati)");
+		System.out.println("3 - Modifica un appuntamento");
+		System.out.println("4 - Cerca un appuntamento per data");
+		System.out.println("5 - Cerca appuntamento per nome");
+		System.out.println("6 - Elimina un appuntamento per data");
+		System.out.println("7 - Elimina appuntamento per nome");
+		System.out.println("8 - Ordina appuntamenti per data");
+		System.out.println("9 - Stampa tutti gli appuntamenti");
+		System.out.println("10 - Scrivi appuntamento su file");
+		System.out.println("11 - Leggi appuntamenti da file");
+		System.out.println("0 - Esci ");
+		System.out.println("\n\n");
+		System.out.print("\n Inserisci un comando: ");
+		System.out.println("\n\n\n");
 	}
 	
-	public static void main(String[] args) throws IOException, NumeroCampiException  {
+	public static Appuntamento nuovoApp(){
+
+		Appuntamento app = new Appuntamento();
+		
+		try {
+		
+			System.out.print("Inserisci il nome della persona:");
+			app.setNome(Input.readString());
+			
+			System.out.print("Inserisci la data (PER ORA AAAA-MM-GG):");
+			String da = Input.readString();
+			String arr[] = da.split("-");
+			app.setData(arr[0], arr[1], arr[2]);
+			
+			System.out.print("Inserisci la durata (in minuti):");
+			app.setDurata(Input.readInt());
+			
+			System.out.print("Inserisci luogo:");
+			app.setLuogo(Input.readString());
+			
+			System.out.print("Inserisci orario:");
+			String o = Input.readString();
+			String arr2[];
+			arr2=o.split(":");
+			app.setOrario(Integer.parseInt(arr2[0]), Integer.parseInt(arr2[1]));
+
+		}
+		catch (DateTimeParseException e) {
+			System.out.print("Data non valida");
+		}
+		catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			System.out.print("Formato Data oppure Ora invalido, data: AAAA-MM-GG , ora: HH:MM");
+		}
+		
+	return app;
+
+	}
+	
+	public static void main(String[] args){
 			
 			int scelta;
+			// determina se è già stato prodotto il menu o no
+			Boolean esistente = false;
 			
-			Agenda agenda=new Agenda();
-			
+			Agenda agenda = new Agenda();
+			/*
 			Appuntamento a = new Appuntamento();
 			a.setNome("pippo");
 			a.setData("1999", "02", "01");
@@ -61,27 +95,12 @@ public class MainClass {
 			
 			agenda.inserisciApp(a);
 			agenda.inserisciApp(b);
-			
+			*/
 			do{
-				System.out.println("GESTIONE RUBRICA DI APPUNTAMENTI");
-				System.out.println("\n\n");
-				System.out.println("1 - Aggiungi un appuntamento");
-				System.out.println("2 - Elimina un appuntamento (tutti i dati)");
-				System.out.println("3 - Modifica un appuntamento");
-				System.out.println("4 - Cerca un appuntamento per data");
-				System.out.println("5 - Cerca appuntamento per nome");
-				System.out.println("6 - Elimina un appuntamento per data");
-				System.out.println("7 - Elimina appuntamento per nome");
-				System.out.println("8 - Ordina appuntamenti per data");
-				System.out.println("9 - Stampa tutti gli appuntamenti");
-				System.out.println("10 - Scrivi appuntamento su file");
-				System.out.println("11 - Leggi appuntamenti da file");
-				System.out.println("0 - Esci ");
-				System.out.println("\n\n");
-				System.out.print("\n Inserisci un comando: ");
+				MainClass.stampaMenu(esistente);
+				esistente = true; 
 				scelta=Input.readInt();
-				System.out.println("\n\n\n");
-				
+
 				switch(scelta){
 				case 1: //aggiungi appuntamento
 					Appuntamento appu = nuovoApp();
@@ -117,11 +136,6 @@ public class MainClass {
 				case 4: //ricerca di appuntamento per data
 					System.out.print("Inserisci la data (PER ORA AAAA-MMM-GG):");
 					String da = Input.readString();
-					String arr[];
-					arr=da.split("-");
-					//LocalDate.parse(da);
-					//System.out.print(da);
-					//a.getData();
 					agenda.cercaPerData(LocalDate.parse(da));
 					break;
 				case 5: //ricerca di appuntamento per nome
